01fceb35fdfb6fb1e2034093ccdcb7c54fc3fb4a
Leon Loeser <leon.loeser@yahoo.de>
2013-09-03 18:54:07 +0200

    fixes

---
+++ ec_common/extracells/localization/Localization.java
@@ -0,0 +1,23 @@
+package extracells.localization;
+
+public enum Localization
+{
+	US("en_US"), GERMAN("de_DE"), CHINESE("zh_CN"), RUSSIAN("ru_RU");
+
+	private final String locale;
+
+	Localization(String locale)
+	{
+		this.locale = locale;
+	}
+
+	public String filename()
+	{
+		return String.format("/assets/extracells/lang/%s.xml", locale);
+	}
+
+	public String locale()
+	{
+		return locale;
+	}
+}
