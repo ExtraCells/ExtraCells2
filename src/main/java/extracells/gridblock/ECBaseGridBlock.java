b7c3eeaa38615d2d488d02e96fb66b737006d90a
Leon Loeser <leon.loeser@yahoo.de>
2014-11-15 15:52:03 +0100

    Version 2.2.3
    Temporary fix for parts only working in Ad-Hoc mode. Doesn't use channels though

--- src/main/java/extracells/gridblock/ECBaseGridBlock.java
+++ src/main/java/extracells/gridblock/ECBaseGridBlock.java
@@ -31,7 +31,7 @@ public class ECBaseGridBlock implements IGridBlock {
 
     @Override
     public EnumSet<GridFlags> getFlags() {
-        return EnumSet.of(GridFlags.REQUIRE_CHANNEL);
+        return EnumSet.noneOf(GridFlags.class); //TODO temporary fix
     }
 
     @Override
