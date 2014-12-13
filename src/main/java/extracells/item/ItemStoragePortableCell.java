a4d298f0cec107472b88defc03b40edaa63c7fad
DrummerMC <drummermods@icloud.com>
2014-12-13 15:19:48 +0100

    EC 2.1.21
    Added RF Support for Items

--- src/main/java/extracells/item/ItemStoragePortableCell.java
+++ src/main/java/extracells/item/ItemStoragePortableCell.java
@@ -3,11 +3,14 @@ package extracells.item;
 import java.util.ArrayList;
 import java.util.List;
 
+import cofh.api.energy.IEnergyContainerItem;
+import cpw.mods.fml.common.Optional;
 import appeng.api.AEApi;
 import appeng.api.config.AccessRestriction;
 import appeng.api.config.Actionable;
 import appeng.api.config.FuzzyMode;
 import appeng.api.config.PowerMultiplier;
+import appeng.api.config.PowerUnits;
 import appeng.api.implementations.items.IAEItemPowerStorage;
 import appeng.api.storage.IMEInventoryHandler;
 import appeng.api.storage.StorageChannel;
@@ -34,7 +37,8 @@ import extracells.registries.ItemEnum;
 import extracells.util.inventory.ECFluidFilterInventory;
 import extracells.util.inventory.ECPrivateInventory;
 
-public class ItemStoragePortableCell extends Item implements IPortableFluidStorageCell, IAEItemPowerStorage {
+@Optional.Interface(iface = "cofh.api.energy.IEnergyContainerItem", modid = "CoFHAPI|energy")
+public class ItemStoragePortableCell extends Item implements IPortableFluidStorageCell, IAEItemPowerStorage, IEnergyContainerItem {
 
 	IIcon icon;
 	
@@ -232,5 +236,46 @@ public class ItemStoragePortableCell extends Item implements IPortableFluidStora
             itemStack.setTagCompound(new NBTTagCompound());
         return itemStack.getTagCompound();
     }
+    
+    @Override
+	@Optional.Method(modid = "CoFHAPI|energy")
+	public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+		if(container == null)
+			return 0;
+		if(simulate){
+			return (int) (getEnergyStored(container) >= maxExtract ? maxExtract : getEnergyStored(container));
+		}else{
+			return (int) PowerUnits.AE.convertTo(PowerUnits.RF, (extractAEPower(container, (PowerUnits.RF.convertTo(PowerUnits.AE, maxExtract)))));
+		}
+	}
 
+	@Override
+	@Optional.Method(modid = "CoFHAPI|energy")
+	public int getEnergyStored(ItemStack arg0) {
+		return (int) PowerUnits.AE.convertTo(PowerUnits.RF, (getAECurrentPower(arg0)));
+	}
+
+	@Override
+	@Optional.Method(modid = "CoFHAPI|energy")
+	public int getMaxEnergyStored(ItemStack arg0) {
+		return (int) PowerUnits.AE.convertTo(PowerUnits.RF, (getAEMaxPower(arg0)));
+	}
+
+	@Override
+	@Optional.Method(modid = "CoFHAPI|energy")
+	public int receiveEnergy(ItemStack container, int maxReceive, boolean simulate) {
+		if(container == null)
+			return 0;
+		if(simulate){
+			double current = PowerUnits.AE.convertTo(PowerUnits.RF, getAECurrentPower(container));
+			double max = PowerUnits.AE.convertTo(PowerUnits.RF, getAEMaxPower(container));
+			if(max - current >= maxReceive)
+				return maxReceive;
+			else
+				return (int) (max - current);
+		}else{
+			int notStored = (int) (PowerUnits.AE.convertTo(PowerUnits.RF, injectAEPower(container, PowerUnits.RF.convertTo(PowerUnits.AE, maxReceive))));
+			return maxReceive - notStored;
+		}
+	}
 }
