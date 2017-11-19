4bfe7fc1fac6600ec6f7f1cb78d7ff29b4f71bc7
DrummerMC <drummermods@icloud.com>
2017-11-19 16:53:53 +0100

    ore dict export bus now suports IItemHandler

--- src/main/scala/extracells/part/PartOreDictExporter.java
+++ src/main/scala/extracells/part/PartOreDictExporter.java
@@ -2,8 +2,11 @@ package extracells.part;
 
 import java.util.List;
 
+import extracells.util.ItemHandlerUtil;
 import extracells.util.MachineSource;
 import extracells.util.StorageChannels;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import org.apache.commons.lang3.StringUtils;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -195,7 +198,15 @@ public class PartOreDictExporter extends PartECBase implements IGridTickable {
 			return null;
 		}
 		IAEItemStack stack = stack0.copy();
-		if (tile instanceof IInventory) {
+		if(tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing.getOpposite())){
+			IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing.getOpposite());
+			ItemStack itemStack = stack.createItemStack();
+			ItemStack notExported = ItemHandlerUtil.insertItemStack(itemHandler, itemStack, true);
+			if (notExported.isEmpty())
+				return stack;
+			else
+				return stack.setStackSize(stack.getStackSize() - notExported.getCount());
+		} else if (tile instanceof IInventory) {
 			if (tile instanceof ISidedInventory) {
 				ISidedInventory inv = (ISidedInventory) tile;
 				for (int i : inv.getSlotsForFace(facing.getOpposite())) {
