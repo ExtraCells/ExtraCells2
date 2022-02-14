8d96b894cc554b87013c8fb87b8d27d6bf6e7a97
bombcar <github@bombcar.com>
2022-02-14 14:47:32 -0600

    Wallsharingrus (#50)
    * Smaller waller
    * Lower Wallrus Size
    * update buildscript
    * better walrus preserves texture aspect ratio very important
    * reduce size further

--- .github/scripts/test-no-error-reports.sh
+++ .github/scripts/test-no-error-reports.sh
@@ -1,22 +1,28 @@
-if [[ -d "run/crash-reports" ]]; then
+#!/usr/bin/env bash
+
+RUNDIR="run"
+CRASH="crash-reports"
+SERVERLOG="server.log"
+
+if [[ -d $RUNDIR/$CRASH ]]; then
   echo "Crash reports detected:"
-  cat $directory/*
+  cat $RUNDIR/$CRASH/crash*.txt
   exit 1
 fi
 
-if grep --quiet "Fatal errors were detected" server.log; then
+if grep --quiet "Fatal errors were detected" $SERVERLOG; then
   echo "Fatal errors detected:"
   cat server.log
   exit 1
 fi
 
-if grep --quiet "The state engine was in incorrect state ERRORED and forced into state SERVER_STOPPED" server.log; then
+if grep --quiet "The state engine was in incorrect state ERRORED and forced into state SERVER_STOPPED" $SERVERLOG; then
   echo "Server force stopped:"
   cat server.log
   exit 1
 fi
 
-if grep --quiet 'Done .+ For help, type "help" or "?"' server.log; then
+if ! grep --quiet -Po '.+Done \(.+\)\! For help, type "help" or "\?"' $SERVERLOG; then
   echo "Server didn't finish startup:"
   cat server.log
   exit 1
